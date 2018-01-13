f14676d73ad05788f4e0304752bf2b68e8e40d1c
Qais Patankar <qaisjp@gmail.com>
2018-01-13 13:46:38 +0000

    Fix usage of pg pool

--- components/Data.js
+++ components/Data.js
@@ -10,28 +10,31 @@ const dbPool = new pg.Pool({
 	idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 });
 
+dbPool.on('error', function(err, client) {
+	// if an error is encountered by a client while it sits idle in the pool
+	// the pool itself will emit an error event with both the error and
+	// the client which emitted the original error
+	// this is a rare occurrence but can happen if there is a network partition
+	// between your application and the database, the database restarts, etc.
+	// and so you might want to handle it and at least log it out
+	console.error('idle client error', err.message, err.stack);
+});
+
 class Data {
   constructor() {
     this.pool = dbPool;
   }
 
-  connect(callback) {
-    console.log("Connecting to PostgreSQL server " + process.env.PGSQL_HOST);
-
-    if (callback == null) {
-      callback = () => {}
-    }
-
-    dbPool.connect().then(callback)
-    .catch(() => {
-      console.log("Unable to connect to database.")
-    });
-
-  }
-
+  // the query method is for passing queries to the pool
   query(text, values, callback) {
     console.log('query:', text, values);
     return this.query(text, values, callback);
+  }
+
+  // the pool also supports checking out a client for
+  // multiple operations, such as a transaction
+  connect(callback) {
+    dbPool.connect(callback);
   }
 }
 
