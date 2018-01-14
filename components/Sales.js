0c0b3ce3921afade2fd071cd75f71478646a36a6
Qais Patankar <qaisjp@gmail.com>
2018-01-14 13:47:05 +0000

    Finish tweaking Sales

--- components/Sales.js
+++ components/Sales.js
@@ -71,22 +71,29 @@ class Sales {
     }
 
     if (req.body.product == "hoodie") {
-      var hu = new HoodieUtils();
-      if (!hu.checkPrice(req.body.productData.code, (req.body.amount + discount))) {
-        var response = {};
+      const hu = new HoodieUtils();
+
+      // Check to see if the amount matches.
+      if (!hu.checkPrice(req.body.productData.code, req.body.amount + discount)) {
+        // Craft an error respnose
+        const response = {};
         response.result = 'error';
         response.success = false;
         response.err_type = "Item_Price_Mismatch";
         response.err_msg = "The price in your request does not match the product's real price.";
+
+        // Send response
         console.log(JSON.stringify(response));
         res.send(JSON.stringify(response));
         return;
       }
     }
 
-    var orderId = shortid.generate();
+    // Generate a pretty much unique order ID
+    const orderId = shortid.generate();
 
-    var qresult = await Data.query(
+    // Attempt into insert the pending transaction
+    let qresult = await Data.query(
       `
         INSERT INTO sales (
           order_id, customer_name, product_details, customer_email,
@@ -94,59 +101,67 @@ class Sales {
         ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
       `,
       [orderId, req.body.productData.customer, req.body.productData, req.body.token.email, req.body.coupon, null, false, req.body.amount]
-      )
-      .then((res) => { return true; })
-      .catch((reason) => {
+    )
+      .then(() => true)
+      .catch(reason => {
         console.log(reason.stack);
-        var response = {};
+
+        const response = {};
         response.result = 'error';
         response.success = false;
         response.err_type = "DB_Write_Error_Precharge";
         response.err_msg = "Error writing order details to database. Your card has not been charged.";
+
+        // Send response
         console.log(JSON.stringify(response));
         res.send(JSON.stringify(response));
         return false;
       });
-    
+
     // Terminate here if error
-    if (qresult === false) {
+    if (!qresult) {
       return;
     }
 
-    var stripeResponse = await new ApiStripe().createCharge(req.body.amount, req.body.token, req.body.productData.description);
+    const stripeResponse = await new ApiStripe().createCharge(req.body.amount, req.body.token, req.body.productData.description);
 
+    // Check if stripe succeeded.
     if (!stripeResponse.success) {
+      Data.query("delete from sales where order_id = $1", [orderId]);
       res.send(JSON.stringify(stripeResponse));
       return;
     }
 
-    var qresult = await Data.query("UPDATE sales SET transaction_id = $1 WHERE (orderId = $2);", [req.body.coupon, orderId])
-      .then((res) => { return true; })
-      .catch(function(reason){
+    qresult = await Data.query("UPDATE sales SET transaction_id = $1 WHERE (order_id = $2);", [req.body.coupon, orderId])
+      .then(() => true)
+      .catch(reason => {
         console.log(reason.stack);
-        var response = {};
+
+        const response = {};
         response.result = 'error';
         response.success = false;
         response.err_type = "DB_Write_Error_Postcharge";
         response.err_msg = "Error marking order as paid. Your card has been charged. Contact hello@comp-soc.com for help quoting order ID " + orderId + " and transaction_id " + stripeResponse.transaction_id + ".";
+
+        // Send response
         console.log(JSON.stringify(response));
         res.send(JSON.stringify(response));
         return false;
       });
     
     // Terminate if error
-    if (qresult === false) {
+    if (!qresult) {
       return;
     }
 
-    Data.query("UPDATE coupons SET used = true WHERE (coupon_code = $1);", [req.body.coupon])
+    Data.query("UPDATE coupons SET used = true WHERE coupon_code = $1;", [req.body.coupon])
       .catch(function(err){
         console.log(err.stack);
         // no need to tell the user about this one... probably better they didn't know.
+        // haha
       });
 
-    var emailContent;
-
+    let emailContent;
     switch (req.body.product){
       case ('hoodie'):
         emailContent = new HoodieUtils().createEmailContent(req.body.productData, discount, orderId, stripeResponse);
@@ -158,14 +173,14 @@ class Sales {
 
     new EmailUtils().sendMail('Thanks for your recent purchase!', emailContent, req.body.token.email);
 
-    var response = {
+    const response = {
       success: true,
       result: 'charge',
       id: stripeResponse.id,
       amount: stripeResponse.amount,
       message: stripeResponse.message,
       outcome_type: stripeResponse.outcome_type,
-      last4: stripeResponse.last4
+      last4: stripeResponse.last4,
     };
     res.send(JSON.stringify(response));
   }
