ce86666e889761cab0f5b021f45b130fba0e8505
Qais Patankar <qaisjp@gmail.com>
2018-01-14 09:42:41 +0000

    Clean up server.js a little

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const app = express();
 var bodyParser = require('body-parser');
-var Sales = require('./components/Sales.js');
+const Sales = require('./components/Sales.js');
+const sales = new Sales();
 
 app.use(bodyParser.json());
 
@@ -12,13 +13,17 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/api', function (req, res) {
+app.get('/api', function(req, res) {
   res.send('This is the backend for the CompSoc website. k thanks bye.')
 });
 
-app.post('/api/sales/charge', function (req, res) { new Sales().charge(req, res); });
+app.post('/api/sales/charge', function(req, res) {
+  sales.charge(req, res);
+});
 
-app.post('/api/sales/coupon', function(req, res) { new Sales().couponCheck(req, res); });
+app.post('/api/sales/coupon', function(req, res) {
+  sales.couponCheck(req, res);
+});
 
 app.get('/api/alive', function(req, res) {
   res.send(JSON.stringify({alive: true, message: "Hi there. This is the CompSoc API endpoint. I am alive."}));
